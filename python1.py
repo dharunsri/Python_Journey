cb6646f67f9ea232d8fc9dd5e31e2ef9ef902de8
Tech Boy <67187601+dharunsri@users.noreply.github.com>
2021-03-17 22:12:40 -1200

    Python #0
    Intro to Python

---
+++ python1.py
@@ -0,0 +1,58 @@
+"""Python:
+     Founded By : Guido Van Rossum
+     Name Reason : He loves a British comedy movie called "Banty Python's flying circus" So he named this language as PYTHON"""
+
+
+""" Python is called as "General Purpose Language"
+    Python is,
+        1. Interpreted
+        2. Object-Oriented
+        3. High-Level  .........  Programming language
+"""
+
+""" Python - 1989        Java - 1995
+    Reddit is fully coded with python (reddit.com)
+"""
+
+""" To program - IDE(Integrated Development Environment - Ex: pycharm)
+    pycharm by jetbrains.... Jetbrains is a industry which build many IDEs
+"""
+
+
+
+print(" HI EVERYONE")
+# o/p : HI EVERYONE
+
+print(" Hi from Mr.D's program")
+# o/p : Hi from Mr.D's program   [to print single quotes use double and vice versa]
+
+print('Hi, this is "Mr.D"')
+#o/p : Hi, this is "Mr.D"        [to print double quotes use single and vice versa]
+
+print(r'c:\docs\Mr.D')
+#o/p : c:\docs\Mr.D              [r - refers to raw data\ raw string. It prints the exact strings which we gave.]
+
+print('I\'m programming a "example" program')
+#o/p : I'm programming a "example" program  [ to print both single and double quotes. \ - refers to ignore that special meaning]
+
+print("Hi" * 3)
+#o/p : HiHiHi                   [ string concatination - print a string multiple times]
+
+print(5/2)
+#o/p : 2.5
+
+print(4//3)
+#o/p : 1                        [ to get integer value on division using floor division (//)]
+
+
+print(2+3)
+#o/p : 5
+
+
+print(2**3)
+#o/p : 8
+
+print(10%3)
+#o/p : 1
+
+
