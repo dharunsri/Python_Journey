61c02061d9c5aa4c10771e47ddfb8ea9a840bfb0
Tech Boy <67187601+dharunsri@users.noreply.github.com>
2021-04-11 17:20:55 -1200

    Create OOPs.py
    OOPs concept explained

---
+++ OOPs.py
@@ -0,0 +1,21 @@
+# The reason why python is famous,
+
+# Because python supports all different programming paradigm
+
+# It supports Functional programming, procedural programming and Object oriented programming
+
+
+# Object,class, method
+
+# In class - we need to declare attributes(variables) and behaviour(methods[functions])
+
+class computer:                         # Class declaration
+
+    def config(self):                   # Method Declaration
+        print('i5','4Gb','1Tb')
+
+com1 = computer()                       # com1 - object 1
+com2 = computer()                       # com2 - object 2
+
+com1.config()
+computer.config(com2)
