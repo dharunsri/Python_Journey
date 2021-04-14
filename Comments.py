3bd5f847200ea0a8318a4e1deb2d014980959ebb
Tech Boy <67187601+dharunsri@users.noreply.github.com>
2021-04-14 07:10:56 -1200

    Create Comments.py
    comments in python explained

---
+++ Comments.py
@@ -0,0 +1,11 @@
+#  There are 2 ways of creating a comment
+
+# #- for single line comment
+
+# """ - for multi line comment
+
+# But """ its mostly not preferrable. It may used to create documentation
+
+# so python has only one line comment
+
+"""hi"""
