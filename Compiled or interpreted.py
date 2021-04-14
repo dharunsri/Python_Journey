5297a66e0206a768d0dad7197d22bd9fbf1aca9b
Tech Boy <67187601+dharunsri@users.noreply.github.com>
2021-04-14 07:10:36 -1200

    Create Compiled or interpreted.py
    about python

---
+++ Compiled or interpreted.py
@@ -0,0 +1,23 @@
+# Python is a compiled or interpreted language?
+
+# python is both compiled and interpreted language
+
+# python 1st gets compiled into bytecode
+
+# then it is interpred by pvm (python virtual machine)
+
+# interpreted - line by line execution
+
+# converting a high level language into bytecode is for portability
+
+# which means it'll support all kind of cpu
+
+# so python 1st gets compiled to get bytecode and then it gets interpreted by pvm and finally it becomes machine code
+
+# cpython - the actual implementation we did all are cpython
+
+# there are more types of python
+# cpython   - the implementation is using c language or the internal work done by c language
+# pypy      - pypy is 4.4 faster executing than python. It is implemented by Rpython
+# IronPython  - "       "           "     .net language
+# jython   -    "       "           "     .java language
